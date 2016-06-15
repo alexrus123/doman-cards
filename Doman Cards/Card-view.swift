84bb9212b84d5a50e031081acf9acccf9e5db96a
Alex Test Dev <alyskovich@gmail.com>
2016-06-15 00:33:34 -0700

    simple UI mockups

---
+++ Doman Cards/Card-view.swift
@@ -0,0 +1,26 @@
+//
+//  Card-view.swift
+//  Doman Cards
+//
+//  Created by Aliaksei Lyskovich on 6/15/16.
+//  Copyright © 2016 Aliaksei Lyskovich. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class Card_ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
