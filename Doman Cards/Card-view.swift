aac80d9cae6725ec8be2d138b6bee83acc4e4e00
Alex Test Dev <alyskovich@gmail.com>
2016-06-16 00:14:15 -0700

    images to the view

--- Doman Cards/Card-view.swift
+++ Doman Cards/Card-view.swift
@@ -12,9 +12,14 @@ import UIKit
 
 class Card_ViewController: UIViewController {
     
+    @IBOutlet weak var tlabel: UILabel!
+    @IBOutlet weak var testimg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tlabel.text = "test test 111"
+        testimg.image = UIImage(named: "alligator")
     }
     
     override func didReceiveMemoryWarning() {
